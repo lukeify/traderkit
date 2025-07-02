b55ce5612a79b804c22795a44a666abfef528195
lukeify <5379845+lukeify@users.noreply.github.com>
2025-07-02 20:46:07 +1200

    Initial commit

---
+++ traderkit/traderkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  traderkitApp.swift
+//  traderkit
+//
+//  Created by Luke on 11/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct traderkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ traderkitTests/traderkitTests.swift
@@ -0,0 +1,17 @@
+//
+//  traderkitTests.swift
+//  traderkitTests
+//
+//  Created by Luke on 11/06/2025.
+//
+
+import Testing
+@testable import traderkit
+
+struct traderkitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
